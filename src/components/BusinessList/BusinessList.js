0bc5bbc23a50c3d90656d8711db7df0e0be22f6e
pavel-kostyanov <kspavel777@gmail.com>
2017-11-24 18:44:40 +0300

    codecademy app ravenous

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './BusinessList.css';
+import Business from '../Busines/Business.js';
+
+class BusinessList extends from React.Component {
+  render(){
+    return (
+      <div className="BusinessList">
+  <!-- Add Business Component here -->
+  <Business />
+  <!-- Add Business Component here -->
+  <Business />
+  <!-- Add Business Component here -->
+  <Business />
+  <!-- Add Business Component here -->
+  <Business />
+  <!-- Add Business Component here -->
+  <Business />
+  <!-- Add Business Component here -->
+  <Business />
+    </div>
+    )
+  }
+}
+export default BusinessList;
