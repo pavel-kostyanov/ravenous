8283eb187ac74931f1ce189e12b36f5ffe695ff8
pavel-kostyanov <kspavel777@gmail.com>
2017-11-29 23:57:15 +0300

    lesson 2 part 2

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -3,10 +3,12 @@ import './BusinessList.css';
 import Business from '../Business/Business.js';
 
 class BusinessList extends React.Component {
+
   render(){
     return (
       <div className="BusinessList">
-          {this.props.businesses.map(item => <Business business={item} />)};       
+{this.props.businesses.map((item, i) => <Business businesses={item} key={i} />)};
+
     </div>
     )
   }
